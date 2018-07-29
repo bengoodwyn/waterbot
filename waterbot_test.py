f2e4453a2065989a426c53b6d0048c5477499429
Ben Goodwyn <bengoodwyn@me.com>
2018-07-29 11:38:18 -0500

    :new: Create trivial skeleton

---
+++ waterbot_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+import waterbot
+
+@pytest.fixture
+def client():
+    waterbot.app.config['TESTING'] = True
+    client = waterbot.app.test_client()
+
+    yield client
+
+def test_empty_db(client):
+    rv = client.get('/')
+    assert b'waterbot' in rv.data
